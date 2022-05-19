e32c2d15816111fecda262e6affc6e76c4412e57
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2022-05-19 12:54:01 -0500

    Added function timer

--- Syntax_Highlighter/SyntaxHighlight.exs
+++ Syntax_Highlighter/SyntaxHighlight.exs
@@ -65,3 +65,16 @@ defmodule Highlighter do
 
 end
 
+# Module that checks a function execution time.
+# Taken from:
+# https://stackoverflow.com/questions/29668635/how-can-we-easily-time-function-calls-in-elixir
+# Example call:
+# Benchmark.measure(fn -> Highlighter.syntaxHighlight("Test_files/example_0.json") end)
+defmodule Benchmark do
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
+end
