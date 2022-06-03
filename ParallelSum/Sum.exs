6a6c4c49943ba00236d1520c5a80f467bc995663
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2022-06-03 09:28:37 -0500

    Added pair pattern matching and automatic core detection

--- ParallelSum/Sum.exs
+++ ParallelSum/Sum.exs
@@ -6,24 +6,24 @@ defmodule Hw.Primes do
         defp checkPrimeAux(_n, i, limit) when i >= limit, do: true
         defp checkPrimeAux(n, i, limit), do: checkPrimeAux(n, i + 1, limit)
 
-    def sum_primes(limit), do: sum_primesAux(0, limit, 0)
+    def sum_primes(limit), do: sum_primesAux({0, limit}, 0)
 
-    defp sum_primesAux(start, limit, res) when start > limit, do: res
-    defp sum_primesAux(start, limit, res) do
+    defp sum_primesAux({start, limit}, res) when start > limit, do: res
+    defp sum_primesAux({start, limit}, res) do
         if checkPrime(start) do
-            sum_primesAux(start + 1, limit, res + start)
+            sum_primesAux({start + 1, limit}, res + start)
         else
-            sum_primesAux(start + 1, limit, res)
+            sum_primesAux({start + 1, limit}, res)
         end
     end
 
-    def sum_primes_parallel(limit, threads) do
+    def sum_primes_parallel(limit, threads \\ System.schedulers) do
         coeficient = div(limit, threads)
         remainder = rem(limit, threads)
-        limits = coeficient+remainder..limit//coeficient
+        limits = Enum.to_list(coeficient+remainder..limit//coeficient)
         starts = [0 | Enum.to_list(coeficient+remainder+1..limit//coeficient)]
-        0..threads-1
-            |>Enum.map(&Task.async(fn -> sum_primesAux((Enum.at(starts, &1)), Enum.at(limits, &1), 0) end))
+        Enum.zip(starts, limits)
+            |>Enum.map(&Task.async(fn -> sum_primesAux(&1, 0) end))
             |>Enum.map(&Task.await(&1))
             |>Enum.sum()
     end
